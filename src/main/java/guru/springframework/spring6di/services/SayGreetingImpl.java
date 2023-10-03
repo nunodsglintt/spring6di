b1a6c4e8cd13823c325452bd7b4141ebfd6d12e9
Nuno Dias <nuno.dias@glintt.com>
2023-10-03 10:49:59 +0100

    added all files

---
+++ src/main/java/guru/springframework/spring6di/services/SayGreetingImpl.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring6di.services;
+
+import guru.springframework.spring6di.services.SayGreeting;
+
+public class SayGreetingImpl implements SayGreeting {
+    @Override
+    public String sayGreeting() {
+        return null;
+    }
+}
